4ec0f0b9074b669105bf95bbac6abda871658c7c
최홍희 <vvshinevv@nhnsoft.com>
2022-02-15 07:47:37 +0900

    @Embeddable, @Embedded 예제 진행

--- src/main/java/hello/HelloMain.java
+++ src/main/java/hello/HelloMain.java
@@ -1,15 +1,9 @@
 package hello;
 
-import hello.project.domain.Child;
-import hello.project.domain.Member;
-import hello.project.domain.Parent;
-import hello.project.domain.Team;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class HelloMain {
     public static void main(String[] args) {
@@ -20,22 +14,6 @@ public class HelloMain {
         tx.begin();
 
         try {
-            Parent parentA = new Parent("parent A");
-            Parent parentB = new Parent("parent B");
-
-            em.persist(parentA);
-            em.persist(parentB);
-
-            Child child1 = new Child("child 1");
-            Child child2 = new Child("child 2");
-
-            child1.setParent(parentA);
-            parentB.addChildren(child2);
-
-            em.flush();
-            em.clear();
-
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
---
+++ src/main/java/hello/user/domain/HelloUser.java
@@ -0,0 +1,46 @@
+package hello.user.domain;
+
+import hello.common.domain.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class HelloUser extends BaseEntity {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Embedded
+    private Period period;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "home_city")),
+            @AttributeOverride(name = "street", column = @Column(name = "home_street")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "home_zipcode"))
+    })
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "work_city")),
+            @AttributeOverride(name = "street", column = @Column(name = "work_street")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "work_zipcode"))
+    })
+    private Address workAddress;
+}
